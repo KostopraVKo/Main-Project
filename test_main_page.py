1db06ce95ef921199bcac536e64fbf2b22721f81
KostopraVKo <kostopravkoko@gmail.com>
2021-11-05 17:02:41 +0300

    Page Object test

--- test_main_page.py
+++ test_main_page.py
@@ -1,16 +1,15 @@
-import time
+'''import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_interface_language(browser):
     browser.get(link)
     assert browser.find_element_by_xpath('//button[@type="submit"]')
-    time.sleep(30)
+    time.sleep(30)'''
+from .pages.main_page import MainPage 
 
-
-
-#def test_guest_can_go_to_login_page(browser):
- #   link = "http://selenium1py.pythonanywhere.com/"
-  #  browser.get(link)
-   # login_link = browser.find_element_by_css_selector("#login_link")
-    #login_link.click()
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
