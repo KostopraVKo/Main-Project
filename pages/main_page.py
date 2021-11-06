613321d70a6fabe94288989df98f7779cd2444f3
KostopraVKo <kostopravkoko@gmail.com>
2021-11-06 13:03:39 +0300

    Navigating between pages

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,9 +1,13 @@
 from .base_page import BasePage
+from .login_page import LoginPage
+from .locators import MainPageLocators, LoginPageLocators
 from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link").click()
+        self.browser.find_element(*MainPageLocators.LOGIN_LINK).click()
+        return LoginPage(browser=self.browser, url=self.browser.current_url)
         
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, '#login_link'), 'Login link is not present'
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), 'Login link is not present'
+    
